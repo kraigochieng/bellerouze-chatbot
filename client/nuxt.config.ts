78567044ba9c802fa133a3f285f8a39265ac90be
kraigochieng <kraigochieng@gmail.com>
2025-09-17 22:55:36 +0300

    fix(client): updated nuxt config to reference env appropriately

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,18 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: "2025-07-15",
+	compatibilityDate: "2025-07-15",
 
-				runtimeConfig: {
-								public: {
-												apiBase: "http://localhost:8000",
-								},
-				},
+	runtimeConfig: {
+		public: {
+			// apiBase: "http://localhost:8000",
+			apiBase: `http://${process.env.SERVER_HOST || "localhost"}:${
+				process.env.SERVER_PORT || 8000
+			}`,
+		},
+	},
 
-				modules: ["@nuxt/ui", "@vueuse/nuxt"],
-				css: ["~/assets/css/main.css"],
-				devtools: { enabled: false },
-				vite: {
-								plugins: [tailwindcss()],
-				},
+	modules: ["@nuxt/ui", "@vueuse/nuxt"],
+	css: ["~/assets/css/main.css"],
+	devtools: { enabled: false },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
